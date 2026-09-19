2cb73b9559c90a908d8d17f2131b9636f0e8ab2e
varun <vp919880@gmail.com>
2026-09-19 14:01:59 +0530

    script.js

--- script.js
+++ script.js
@@ -318,6 +318,15 @@ function goBackToCount() {
     ).classList.remove("hidden");
 
 }
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
 
 
 /*
